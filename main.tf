e5e21c43c8916867b0c0791a0af07ef79d667293
Shahid Bashir <mabdulba@akamai.com>
2025-09-30 09:52:02 -0400

    Homework 1

--- main.tf
+++ main.tf
@@ -1,8 +1,28 @@
 data "akamai_group" "my_group_id" {
-    group_name  = "test"
+    group_name  = "Akamai Test"
     contract_id = "1-3CV382"
 }
 
-output "my_groups" {
-  value = data.akamai_groups.my_group_id.id
+output "my_group_id" {
+  value = data.akamai_group.my_group_id.id
+}
+
+
+data "akamai_appsec_security_policy" "my-security-policy" {
+    config_id            = 20308
+    security_policy_name = "aaa"
+}
+
+output "my-security-policy" {
+  value = data.akamai_appsec_security_policy.my-security-policy.id
+}
+
+data "akamai_property" "my_property" {
+  name = "testamd"
+  version     = 12
+}
+
+output "my_property" {
+  #value = data.akamai_property.my_property.property_id
+  value = data.akamai_property.my_property.group_id
 }
