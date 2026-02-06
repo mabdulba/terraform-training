f4aa489d95a7d2f748eec416a913d3aae5c8a548
Shahid Bashir <mabdulba@akamai.com>
2026-02-06 16:21:34 -0500

    Data Source TF

---
+++ main.tf
@@ -0,0 +1,33 @@
+data "akamai_group" "my_group_id" {
+    group_name  = "Akamai Test"
+    contract_id = "1-3CV382"
+}
+
+output "my_group_id" {
+    value = data.akamai_group.my_group_id.id 
+}
+
+
+data "akamai_appsec_security_policy" "my-security-policy" {
+    config_id            = 20308
+    security_policy_name = "aaa"
+}
+
+output "configuration-list" {
+  value = data.akamai_appsec_security_policy.my-security-policy.id
+}
+
+data "akamai_property" "my_property" {
+  name = "testamd"
+  version     = 12
+}
+
+output "my_property" {
+    value = data.akamai_property.my_property.name
+    
+}
+
+output "my_property1" {
+    
+    value = data.akamai_property.my_property.note
+}
